8ddaa387d1c83bc61c5f40cdc6ee78ccc697f3f8
primekrieger <primekrieger@gmail.com>
2015-08-19 12:16:38 +0530

    Collection test

---
+++ Photo Gallery/AlbumsCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  AlbumsCollectionViewController.h
+//  Photo Gallery
+//
+//  Created by Diwakar Kamboj on 19/08/15.
+//  Copyright (c) 2015 Nagarro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlbumsCollectionViewController : UICollectionViewController
+
+@end
