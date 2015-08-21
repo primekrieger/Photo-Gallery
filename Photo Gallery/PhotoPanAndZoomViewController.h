5b4bb3477b17a42b338762a73fd0715f0f170200
primekrieger <primekrieger@gmail.com>
2015-08-21 14:30:02 +0530

    Begin Pan and Zoom

--- Photo Gallery/PhotoPanAndZoomViewController.h
+++ Photo Gallery/PhotoPanAndZoomViewController.h
@@ -12,4 +12,6 @@
 
 @property (strong, nonatomic) IBOutlet UIScrollView *photoScrollView;
 
+@property NSString *selectedPhotoPath;
+
 @end
--- Photo Gallery/PhotosCollectionViewController.m
+++ Photo Gallery/PhotosCollectionViewController.m
@@ -8,6 +8,7 @@
 
 #import "PhotosCollectionViewController.h"
 #import "GalleryData.h"
+#import "PhotoPanAndZoomViewController.h"
 
 @interface PhotosCollectionViewController ()
 
@@ -41,15 +42,18 @@ static NSString * const reuseIdentifier = @"PhotoCell";
     // Dispose of any resources that can be recreated.
 }
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    if ([[segue identifier] isEqualToString:@"showSelectedPhoto"]) {
+        NSIndexPath *indexPath = [[self.collectionView indexPathsForSelectedItems] objectAtIndex:0];
+        NSString *imageName = [[self.currentAlbum objectForKey:@"AlbumImages"] objectAtIndex:indexPath.item];
+        [[segue destinationViewController] setSelectedPhotoPath:[NSString stringWithFormat:@"Photos/%@/%@", self.albumName, imageName]];
+    }
 }
-*/
+
 
 #pragma mark <UICollectionViewDataSource>
 
